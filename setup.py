fa6d0d11349952d32a3822eeaa4944ab8f16f2a7
Matheus Cardoso <matheus.mcas@gmail.com>
2016-03-02 18:11:35 -0300

    gettin mad. I dunno why isnt workin. ~~

--- setup.py
+++ setup.py
@@ -33,18 +33,18 @@ def find_version(*file_paths):
     raise RuntimeError("Unable to find version string.")    
 
 setup(
-    name='django-tastypie-simple-api-doc',
+    name='tastypie_api_doc',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
     version=find_version('tastypie_api_doc','__init__.py'),
 
-    description='A Django app to generate a simple and automatic api documentation with Tastypie',
+    description='A HMAC class authentication for Tastypie and Django',
     long_description=long_description,
 
     # The project's main homepage.
-    url='https://github.com/matheuscas/django-tastypie-simple-api-doc',
+    url='',
 
     # Author details
     author='Matheus Cardoso',
@@ -68,41 +68,29 @@ setup(
 
         'Environment :: Web Environment',
         'Framework :: Django',
-        'Framework :: Django :: 1.8',
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
     ],
 
     # What does your project relate to?
-    keywords='django tastypie api documentation automatic',
+    keywords='django tastypie authentication hmac sha256',
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    # packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-    packages=['tastypie_api_doc'],
-    package_data={'tastypie_api_doc': ['templates/index.html', 
-                                'static/tastypie_api_doc/bower_components/jquery/dist/*.js',
-                                'static/tastypie_api_doc/bower_components/semantic/dist/*.js',
-                                'static/tastypie_api_doc/bower_components/semantic/dist/*.css',
-                                'static/tastypie_api_doc/bower_components/semantic/dist/components/*.js',
-                                'static/tastypie_api_doc/bower_components/semantic/dist/components/*.css']},
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['django-tastypie>=0.12.1','django>=1.8'],
+    install_requires=['django-tastypie>=0.12.1'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
